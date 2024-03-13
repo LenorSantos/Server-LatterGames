d56e2fcbb396ca97dc1ebe64117dc02ab00f7fae
Fabricio <monteirof700@gmail.com>
2024-03-13 17:05:38 -0300

    promo complete and modifications in work mode

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json({limit: '10mb'}));
 app.use(fileupload());
 
+// nota, todos os arquivos de banco nosql devem comecar com [] para poder dar inicio corretamente
+
 // datetime
 app.post("/datetime", (req, res) => {
     console.log(req.body);
@@ -25,39 +27,18 @@ app.get("/pulldatetime", (req, res) => {
                     console.log(err);
                 } else {
                     res.end(data);
-                }
+                };
             } catch (error) {
                 res.end();
-            }
+            };
         });
     } catch (error) {
         res.end(`${error}`);
-    }
+    };
 });
 // datetime
 
 // products
-var dataimage;
-app.post('/image', (req, res) => {
-    dataimage = null;
-    try {
-        const image = req.files.image;
-        image.mv(`./img/products/${image.name}`, () => {
-            // the image is enconded
-            fs.readFile(`./img/products/${image.name}`, 'base64', (err, data) => {
-                if (err) {
-                    console.log("readfile" + err);
-                } else {
-                    dataimage = data;
-                }
-            });
-        });
-        res.end();
-    } catch (error) {
-        res.end(`${error}`);
-    }
-});
-
 app.post('/sendpromos', (req, res) => {
     try {
         var dataproducts = [req.body];
@@ -71,40 +52,37 @@ app.post('/sendpromos', (req, res) => {
                     const alterdata = JSON.parse(data);
                     alterdata.push(dataproducts[0]);
                     fs.writeFile('mapmainpage/products.json', `${JSON.stringify(alterdata)}`, { flag: 'w' }, (err) => {
+                        fs.rm(`./temp/${req.files.img.name}`, (err) => {if (err) throw new Error(err)});
                         if (err) throw new Error(err);
-                        res.end();
+                        res.status(201).end();
                     });
                 });
-                fs.rm(`./temp/${req.files.img.name}`, (err) => {if (err) throw new Error(err)});
             });
         });
     } catch (err) {
-        res.send(err ?? "Save promo error");
-    }
+        res.status(400).send(err ?? "Save promo error");
+    };
     
 });
 
-app.post('/deleteproducts', (req, res) => {
+app.delete('/delproducts/:title', (req, res) => {
     try {
         fs.readFile('mapmainpage/products.json', (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                const alterdata = JSON.parse(data);
-                for (count = 0; count < alterdata.length; count++) {
-                    if (req.body.id == alterdata[count].id) {
-                        alterdata.splice(count, 1);
-                        fs.writeFile('mapmainpage/products.json' ,`${JSON.stringify(alterdata)}`, {flag: 'w'}, (err) => {
-                            if (err) console.log(err);
-                        });
-                    }
-                }
-            }
+            if (err) throw new Error(err);
+            const alterdata = JSON.parse(data);
+            alterdata.forEach((element, index) => {
+                if (req.params.title === element.title) {
+                    alterdata.splice(index, 1);
+                    fs.writeFile('mapmainpage/products.json', `${JSON.stringify(alterdata)}`, { flag: 'w' }, (err) => {
+                        if (err) throw new Error(err);
+                        res.status(200).send("deletado");
+                    });
+                };
+            });
         });
-        res.end();
-    } catch (error) {
-        res.end(`${error}`);
-    }
+    } catch (err) {
+        res.status(400).send(err);
+    };
     
 });
 
@@ -114,19 +92,18 @@ app.get("/promos", (req, res) => {
             try {
                 var predata = new Array(JSON.parse(data));
                 if (err) {
-                    console.log(err);
+                    if (err) throw new Error(err);
                 } else if (predata[0].length === 0) {
-                    res.end("{}");
+                    res.status(204).send("{}");
                 } else {
-                    res.end(data);
+                    res.status(200).send(data);
                 }
-            } catch (error) {
-                console.log(error)
-                res.end();
+            } catch (err) {
+                res.status(400).send(err);
             }
         });
-    } catch (error) {
-        res.end(`${error}`);
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 // products
