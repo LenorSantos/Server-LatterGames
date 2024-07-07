2c602645d7c621dacd83560b2adfa6ac3cfbf3fa
LenorSantos <monteirof700@gmail.com>
2024-07-07 10:23:49 -0300

    latest main version

--- src/datetime/setdatetime.js
+++ src/datetime/setdatetime.js
@@ -1,10 +1,11 @@
 const setDateTime = (req, res, fs) => {
     try {
-        console.log(req.body);
-        fs.writeFile('src/mapmainpage/datetime.json', `${JSON.stringify(req.body)}`, { flag: 'w' }, (err) => {
-            if (err) throw new Error(err);
-            res.status(200).end();
-        });
+        // console.log(req);
+        // fs.writeFile('src/mapmainpage/datetime.json', `${JSON.stringify(req.body)}`, { flag: 'w' }, (err) => {
+        //     if (err) throw new Error(err);
+        //     res.status(200).end();
+        // });
+        res.status(200).end();
     } catch (err) {
         res.status(400).send(err);
     }
