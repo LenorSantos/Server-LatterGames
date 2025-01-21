34fa9b4158caf180e5565b4d2ad099c3da9b6a6f
LenorSantos <monteirof700@gmail.com>
2025-01-20 23:13:19 -0300

    Ajustado caminho da pasta temporaria

--- src/news/setNews.js
+++ src/news/setNews.js
@@ -1,9 +1,9 @@
 const setNews = (req, res, uniqid, fs) => {
     try {
         var datanews = [req.body];
-        req.files.img.mv(`src/temp/${req.files.img.name}`, (err) => {
+        req.files.img.mv(`temp/${req.files.img.name}`, (err) => {
             if (err) throw new Error(err);
-            fs.readFile(`src/temp/${req.files.img.name}`, 'base64', (err, data) => {
+            fs.readFile(`temp/${req.files.img.name}`, 'base64', (err, data) => {
                 if (err) throw new Error(err);
                 datanews[0]['id'] = `${uniqid(req.body.title)}`;
                 datanews[0]['imgdata'] = `data:image/png;base64,${data}`;
@@ -12,7 +12,7 @@ const setNews = (req, res, uniqid, fs) => {
                     const alterdata = JSON.parse(data);
                     alterdata.push(datanews[0]);
                     fs.writeFile('src/mapmainpage/news.json', `${JSON.stringify(alterdata)}`, { flag: 'w' }, (err) => {
-                        fs.rm(`src/temp/${req.files.img.name}`, (err) => {if (err) throw new Error(err)});
+                        fs.rm(`temp/${req.files.img.name}`, (err) => {if (err) throw new Error(err)});
                         if (err) throw new Error(err);
                         res.status(201).end();
                     });
--- src/promos/sendpromos.js
+++ src/promos/sendpromos.js
@@ -1,9 +1,9 @@
 const sendPromos = (req, res, uniqid, fs) => {
     try {
         var dataproducts = [req.body];
-        req.files.img.mv(`src/temp/${req.files.img.name}`, (err) => {
+        req.files.img.mv(`temp/${req.files.img.name}`, (err) => {
             if (err) throw new Error(err);
-            fs.readFile(`src/temp/${req.files.img.name}`, 'base64', (err, data) => {
+            fs.readFile(`temp/${req.files.img.name}`, 'base64', (err, data) => {
                 if (err) throw new Error(err);
                 dataproducts[0]['id'] = `${uniqid(req.body.title)}`;
                 dataproducts[0]['imgdata'] = `data:image/png;base64,${data}`;
@@ -12,7 +12,7 @@ const sendPromos = (req, res, uniqid, fs) => {
                     const alterdata = JSON.parse(data);
                     alterdata.push(dataproducts[0]);
                     fs.writeFile('src/mapmainpage/products.json', `${JSON.stringify(alterdata)}`, { flag: 'w' }, (err) => {
-                        fs.rm(`src/temp/${req.files.img.name}`, (err) => {if (err) throw new Error(err)});
+                        fs.rm(`temp/${req.files.img.name}`, (err) => {if (err) throw new Error(err)});
                         if (err) throw new Error(err);
                         res.status(201).end();
                     });
