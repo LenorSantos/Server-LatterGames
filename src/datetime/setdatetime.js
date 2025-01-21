28d87f26f5a7cbc9882a2162423acf4cf20f4852
LenorSantos <monteirof700@gmail.com>
2025-01-20 23:12:20 -0300

    Liberado edição da hora e data

--- src/datetime/setdatetime.js
+++ src/datetime/setdatetime.js
@@ -1,10 +1,13 @@
-const setDateTime = (req, res, fs) => {
+const {request, response} = require("express");
+const fs = require("fs");
+// criar uma class para evitar requisitar o express e o fs em todos os arquivos aparenta ser a melhor solução
+const setDateTime = (req = request, res = response) => {
     try {
-        // console.log(req);
-        // fs.writeFile('src/mapmainpage/datetime.json', `${JSON.stringify(req.body)}`, { flag: 'w' }, (err) => {
-        //     if (err) throw new Error(err);
-        //     res.status(200).end();
-        // });
+        console.log(req.body);
+        fs.writeFile('src/mapmainpage/datetime.json', `${JSON.stringify(req.body)}`, { flag: 'w' }, (err) => {
+            if (err) throw new Error(err);
+            res.status(200).end();
+        });
         res.status(200).end();
     } catch (err) {
         res.status(400).send(err);
